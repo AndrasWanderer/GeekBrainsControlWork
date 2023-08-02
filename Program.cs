6ddcbbd56d57b315c4c712196290daec28718079
Andras <warlock61636@yandex.ru>
2023-08-02 20:38:08 +0300

    Решение задачи

--- Program.cs
+++ Program.cs
@@ -4,3 +4,22 @@
 Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 */
+
+
+Console.WriteLine("Введите элементы массива (через запятую):");
+string[] initialArray = Console.ReadLine().Split(',');
+string[] newArray = new string[initialArray.Length];
+int newIndex = 0;
+for (int i = 0; i < initialArray.Length; i++)
+{
+    if (initialArray[i].Length <= 3)
+    {
+        newArray[newIndex] = initialArray[i];
+        newIndex++;
+    }
+}
+string[] finalArray = new string[newIndex];
+Array.Copy(newArray, finalArray, newIndex);
+Console.WriteLine("Результат:");
+foreach (string element in finalArray)
+    Console.Write(element + ", ");
